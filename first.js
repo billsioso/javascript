fb44adb6c343292a787602edfecc48a262b6470b
Bill <dariennx@hotmail.com>
2020-12-01 12:07:03 -0300

    functions

--- first.js
+++ first.js
@@ -1,6 +1,24 @@
-let saludar =(nombre) =>{
-    let frase =  `Hola ${nombre}`;
-    document.write(nombre);
+let gratis = true;
+
+let entrada = (hora) =>{
+    let edad = prompt("Qué edad tenés?");
+    
+    if (edad < 18){
+       alert("no pasas");
+    }
+    else{
+        if(edad >=18 && hora== 2 && gratis == true){
+            alert ("que suerte, sos el primero y no pagás, pasas nomás");
+            gratis = false;
+        }
+            
+        else if (edad >=18){
+            alert("pasas pero pagas");
+        }
+    }
+
 }
 
-saludar("Bill");
+entrada(3);
+entrada(2);
+entrada(2);
