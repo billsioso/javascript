029d1cb28471e58ce579820e8d78877e7ac5573a
Bill <dariennx@hotmail.com>
2020-12-14 00:19:10 -0300

    onclick

--- first.js
+++ first.js
@@ -1,24 +1,3 @@
-let gratis = true;
-
-let entrada = (hora) =>{
-    let edad = prompt("Qué edad tenés?");
-    
-    if (edad < 18){
-       alert("no pasas");
-    }
-    else{
-        if(edad >=18 && hora== 2 && gratis == true){
-            alert ("que suerte, sos el primero y no pagás, pasas nomás");
-            gratis = false;
-        }
-            
-        else if (edad >=18){
-            alert("pasas pero pagas");
-        }
-    }
-
-}
-
-entrada(3);
-entrada(2);
-entrada(2);
+numeros = [1,2,3];
+numeros.reverse();
+document.write(numeros);
